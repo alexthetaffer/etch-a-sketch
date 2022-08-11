8e4a1fc677b913d1f9a50cf345acfc47c9acad14
alex <alexthetaffer@gmail.com>
2022-08-11 14:02:17 +0300

    Add change pixel color logic

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const CANVAS_SIZE = 960;
 const canvas = document.querySelector('.canvas');
+let pixels;
+let color = 'black';
+const size16 = document.querySelector('#size-16');
+const size32 = document.querySelector('#size-32');
+const size64 = document.querySelector('#size-64');
+
+createCanvas(16);
 
 function createCanvas(n) {
     const pixelSize = Math.floor(CANVAS_SIZE / n);
@@ -10,5 +17,40 @@ function createCanvas(n) {
         pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px`;
         canvas.appendChild(pixel);
     }
+    const pixels = document.querySelectorAll('.pixel');
+    
+    // Change pixels color
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', () => changeColor(pixel, color));
+    })
 }
-createCanvas(16);
+
+function deleteCanvas() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        canvas.removeChild(pixel);
+    })
+}
+
+function changeColor(pixel, color) {
+    pixel.style.backgroundColor = color;
+}
+
+// Change canvas size
+size16.addEventListener('click', () => {
+    deleteCanvas();
+    createCanvas(16);
+});
+
+size32.addEventListener('click', () => {
+    deleteCanvas();
+    createCanvas(32);
+});
+
+size64.addEventListener('click', () => {
+    deleteCanvas();
+    createCanvas(64);
+});
+
+
+
